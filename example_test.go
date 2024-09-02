42fd49322d8de006763d3524f71a889af10ad198
Aston Motes <astonm@gmail.com>
2024-09-02 11:13:50 -0400

    Add documentation and examples for a few functions

---
+++ example_test.go
@@ -0,0 +1,40 @@
+package itertools_test
+
+import (
+	"fmt"
+
+	it "github.com/astonm/go-itertools"
+)
+
+func ExampleNewSeq() {
+	for v := range it.NewSeq(1, 2, 3) {
+		fmt.Println(v)
+	}
+
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func ExampleFromSlice() {
+	for v := range it.FromSlice([]int{1, 2, 3}) {
+		fmt.Println(v)
+	}
+
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func ExampleEnumerate() {
+	for i, v := range it.Enumerate(it.NewSeq("a", "b", "c")) {
+		fmt.Println(i, v)
+	}
+
+	// Output:
+	// 0 a
+	// 1 b
+	// 2 c
+}
